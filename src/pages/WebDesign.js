a275f164efdf227f1cca5a86bf85294a6ce79c2c
Héctor Reyes <h.reyes@idventiva.com>
2020-07-24 22:36:56 -0500

    Boton Figma

--- src/pages/WebDesign.js
+++ src/pages/WebDesign.js
@@ -1,12 +1,20 @@
 import React, { Fragment } from "react";
+
 import Title from "../components/Title";
 import Intro from "../components/Intro";
+import SecondarySubtitle from "../components/SecondarySubtitle";
+import Button from "../components/Button";
 
 export const WebDesign = () => {
   return (
     <Fragment>
       <Title title="Diseño Web" />
-      <Intro intro="Hola, soy una descripción muy cool" />
+      <Intro intro="Arquitectura de información, Wireframing y prototipado de la aplicación." />
+      <SecondarySubtitle value="Diseño de Interfaz" />
+      <Button
+        value="Diseño de Interfaz - Figma"
+        url="https://www.figma.com/file/YbqbeMuP86KX8xNceTOCXT/Comeet-Cat?node-id=79%3A234"
+      />
     </Fragment>
   );
 };
